e44fb457ccf4e99b0bd14315db15f747620c487d
Fernández Alejandro <137321795+fernandezalejandro@users.noreply.github.com>
2025-04-23 20:40:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var typed = new Typed('#element', {
     strings: [
-        '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Full Stack</span>',
+        '<span class="texto-neon-blanco texto-contorno">Desarrollo </span><span class="texto-neon">Full Stack</span>',
         '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Python3</span>',
         '<span class="texto-contorno">Experiencia </span><span class="texto-neon">Pentesting</span>',
         '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Scripting</span>',
         '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Full Stack</span>'
     ],
     typeSpeed: 60,
-    showCursor: true, // Asegura que el cursor sea visible
-    cursorChar: '|',  // Puedes personalizar el carácter del cursor si lo deseas
-    backSpeed: 60,    // Velocidad al borrar
-    backDelay: 1000,  // Tiempo de espera antes de borrar
+    showCursor: true,
+    cursorChar: '|',
+    backSpeed: 60,
+    backDelay: 1000,
 });
