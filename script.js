e6bb8fd1944f8f3ee8680814a8d6143db40e92e2
Fernández Alejandro <137321795+fernandezalejandro@users.noreply.github.com>
2025-04-23 20:00:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-const frases = [
-    "Desarrollo <span class='neon-text-red'>Full Stack</span>",
-    "Desarrollo <span class='neon-text-red'>Python3</span>",
-    "Experiencia en <span class='neon-text-red'>CiberSeguridad</span>",
-    "Desarrollo de <span class='neon-text-red'>Scripting</span>"
-];
-
-const texto = document.getElementById("texto");
-let i = 0;
-let j = 0;
-let escribiendo = true;
-
-function efectoMaquina() {
-    const fraseActual = frases[i];
-    if (escribiendo) {
-        texto.innerHTML = fraseActual.slice(0, j++); // Usamos innerHTML para poder mostrar el HTML de los spans
-        if (j > fraseActual.length) {
-            escribiendo = false;
-            setTimeout(efectoMaquina, 1000); 
-            return;
-        }
-    } else {
-        texto.innerHTML = fraseActual.slice(0, --j); // Usamos innerHTML para borrar correctamente el texto
-        if (j === 0) {
-            escribiendo = true;
-            i = (i + 1) % frases.length;
-        }
-    }
-    setTimeout(efectoMaquina, escribiendo ? 100 : 50);
-}
-
-efectoMaquina();
+var typed = new Typed('#element', {
+    strings: [
+        '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Full Stack</span>',
+        '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Python3</span>',
+        '<span class="texto-contorno">Experiencia en </span><span class="texto-neon">CiberSeguridad</span>',
+        '<span class="texto-contorno">Desarrollo de </span><span class="texto-neon">Scripting</span>',
+        '<span class="texto-contorno">Desarrollo </span><span class="texto-neon">Full Stack</span>'
+    ],
+    typeSpeed: 60,
+    showCursor: true, // Asegura que el cursor sea visible
+    cursorChar: '|',  // Puedes personalizar el carácter del cursor si lo deseas
+    backSpeed: 60,    // Velocidad al borrar
+    backDelay: 1000,  // Tiempo de espera antes de borrar
+});
